554c2c65b5a5f533dbc8fec122e1f6a3cf5a9a03
Fsepulvedap <fsepulveda.87@gmail.com>
2024-11-12 00:19:41 +0000

    Proyecto terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
 const Home = () => {
+	const [item, setItem] = useState("")
+	const [toDos, setToDos] = useState([])
+
+
+	const handleEnter = async (e) => {
+		if (e.key === "Enter") {
+			fetch("https://playground.4geeks.com/todo/todos/usuariofs001", {
+				method: "POST",
+				body: JSON.stringify({
+					"label": item,
+					"is_done": false
+				}),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+				.then((res) => {
+					if (res.ok) console.log("Tarea agregada correctamente")
+				})
+			setItem("")
+			await handleGetTodos()
+		}
+	}
+
+	const handleGetTodos = async () => {
+		try {
+			const res = await fetch("https://playground.4geeks.com/todo/users/usuariofs001")
+			const data = await res.json()
+			if (res.ok) setToDos(data.todos)
+		} catch (error) {
+			console.log(error)
+		}
+
+	}
+
+	const handleDelete = (id) => {
+		fetch("https://playground.4geeks.com/todo/todos/" + id,
+			{
+				method: "DELETE"
+			})
+			.then((res) => {
+				if (res.ok) console.log("Tarea eliminada correctamente")
+			})
+		
+	}
+
+	useEffect(() => {
+		handleGetTodos()
+
+	}, [handleDelete])
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>todos</h1>
+			<div className="lista-contenedor">
+				<ul>
+					<li><input type="text" placeholder="Que falta hacer?" onChange={(e) => setItem(e.target.value)} value={item} onKeyDown={(e) => handleEnter(e)}></input> </li>
+					{toDos && toDos.map((item, index) => (
+						<li key={index}> {item.label} <i className="fa fa-times" onClick={() => handleDelete(item.id)}></i></li>
+					))}
+					<li className="faltantes"> {toDos.length} tareas faltantes </li>
+				</ul>
+			</div>
 		</div>
 	);
 };
